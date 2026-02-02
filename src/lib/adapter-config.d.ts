ac42daff1e255293d66190a4c3cc05106907f295
Garfonso <garfonso@mobo.info>
2026-02-02 13:41:25 +0100

    adjust to places and fences

--- src/lib/adapter-config.d.ts
+++ src/lib/adapter-config.d.ts
@@ -7,6 +7,15 @@ declare global {
             googleUsername: string;
             googlePassword: string;
             pollInterval: number;
+            placesInstance: string;
+            fences: Array<{
+                name: string;
+                latitude: number;
+                longitude: number;
+                radius: number;
+                user: string;
+                fenceId: string;
+            }>;
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@
 // you need to create an adapter
 import * as utils from '@iobroker/adapter-core';
 
-import puppeteer from 'puppeteer';
-import type { Browser } from 'puppeteer';
+import { User } from './lib/User';
+import { Fence } from './lib/Fence';
+import { Cookie } from './lib/Cookie';
 import axios from 'axios';
 
 //used to test timeout against
@@ -16,13 +17,22 @@ const MAX_INT32 = 2 ** 31 - 1; // 2147483647 (hex 0x7FFFFFFF)
 // Load your modules here, e.g.:
 // import * as fs from 'fs';
 
-class GoogleSharedlocations2 extends utils.Adapter {
-    _cookies: string | null = null;
+/**
+ * The adapter class
+ */
+export class GoogleSharedlocations2 extends utils.Adapter {
     _pollTimeout: ioBroker.Timeout | undefined;
     _pollInterval: number = 300;
     _successFullPolls: number = 1; // let us try a relogin at start, if cookie does not work.
-    _browser: Browser | null = null;
+    _users: Record<string, User> = {};
+    fences: Fence[] = [];
+    cookie: Cookie;
 
+    /**
+     * Creates an instance of the adapter.
+     *
+     * @param options - adapter options
+     */
     public constructor(options: Partial<utils.AdapterOptions> = {}) {
         super({
             ...options,
@@ -31,8 +41,9 @@ class GoogleSharedlocations2 extends utils.Adapter {
         this.on('ready', this.onReady.bind(this));
         this.on('stateChange', this.onStateChange.bind(this));
         // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
+        this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
+        this.cookie = new Cookie(this);
     }
 
     /**
@@ -43,15 +54,7 @@ class GoogleSharedlocations2 extends utils.Adapter {
 
         // Reset the connection indicator during startup
         await this.setState('info.connection', false, true);
-        this._cookies = ((await this.getStateAsync('info.currentCookies'))?.val as string) || null;
-        if (!this._cookies) {
-            if (!this.config.googleUsername || !this.config.googlePassword) {
-                this.log.error('Google username or password not set in adapter configuration!');
-                return;
-            }
-            await this.loginToGetNewCookies();
-        }
-
+        await this.cookie.init();
         await this.subscribeStatesAsync('info.currentCookies');
 
         //sanitize polling interval:
@@ -66,20 +69,67 @@ class GoogleSharedlocations2 extends utils.Adapter {
             this._pollInterval = MAX_INT32;
         }
 
+        //read fences:
+        for (const fenceConfig of this.config.fences || []) {
+            const fence = new Fence(
+                fenceConfig.name,
+                fenceConfig.latitude,
+                fenceConfig.longitude,
+                fenceConfig.radius,
+                fenceConfig.user,
+                fenceConfig.fenceId,
+            );
+            if (fence.valid) {
+                this.fences.push(fence);
+                await this.setObjectNotExistsAsync(`fences.${fence.fenceId}`, {
+                    type: 'state',
+                    common: {
+                        name: `${fence.name}`,
+                        type: 'boolean',
+                        read: true,
+                        write: false,
+                        role: 'sensor',
+                    },
+                    native: {},
+                });
+            } else {
+                this.log.warn(`Fence ${fenceConfig.name} is not valid and will be ignored.`);
+            }
+        }
+        //clear old fences:
+        const adapterObjects = await this.getAdapterObjectsAsync();
+        for (const objId of Object.keys(adapterObjects)) {
+            if (objId.startsWith(`${this.namespace}.fences.`) || objId.startsWith('fences.')) {
+                const fenceId = objId.split('.').pop() || '';
+                const found = this.fences.find(f => f.fenceId === fenceId);
+                if (!found) {
+                    this.log.info(`Deleting old fence state ${objId} as it is not in configuration anymore.`);
+                    await this.delObjectAsync(objId);
+                }
+            }
+        }
+
         //start polling positions
         this.pollPositions();
-        if (this._cookies) {
+        if (this.cookie.isValid()) {
             await this.sendRequest();
         }
     }
 
     private pollPositions(): void {
         this._pollTimeout = this.setTimeout(async () => {
-            if (!this._cookies) {
+            if (!this.cookie.isValid()) {
                 this.log.debug('Cannot poll positions, no cookies available!');
             } else {
                 this.log.debug('Polling positions with current cookies.');
+                const lastSuccessPolls = this._successFullPolls;
                 await this.sendRequest();
+                if (this._successFullPolls > 0 && lastSuccessPolls !== this._successFullPolls) {
+                    if (this._successFullPolls % 10 === 0) {
+                        //try to get some more headers from google:
+                        await this.cookie.improveCookie();
+                    }
+                }
             }
             //schedule next poll
             return this.pollPositions();
@@ -87,7 +137,7 @@ class GoogleSharedlocations2 extends utils.Adapter {
     }
 
     private async sendRequest(): Promise<void> {
-        if (!this._cookies) {
+        if (!this.cookie.isValid()) {
             this.log.error('Cannot send request, no cookies available!');
             await this.setState('info.connection', false, true);
             return;
@@ -99,7 +149,7 @@ class GoogleSharedlocations2 extends utils.Adapter {
             method: 'GET',
             url: 'https://www.google.com/maps/rpc/locationsharing/read',
             headers: {
-                Cookie: this._cookies,
+                Cookie: this.cookie.currentCookie,
             },
             params: {
                 authuser: 2,
@@ -120,71 +170,36 @@ class GoogleSharedlocations2 extends utils.Adapter {
                 this._successFullPolls += 1;
                 await this.setState('info.connection', true, true);
                 for (const location of locations) {
-                    await this.fillIntoObjects(location);
+                    const user = new User(location);
+                    if (user.id) {
+                        this._users[user.id] = user;
+                        await this.fillIntoObjects(user);
+                        await this.notifyPlaces(user);
+                        await this.checkFences(user);
+                    }
                 }
             } else {
                 this.log.info('No shared locations found in the response, probably not logged in.');
                 if (this._successFullPolls > 0) {
                     //try to get new cookie:
-                    await this.loginToGetNewCookies();
+                    this._successFullPolls = 0;
+                    await this.cookie.loginToGetNewCookies();
                 }
             }
         } catch (e) {
             this.log.error(`Error during request: ${(e as Error).message}`);
             if (this._successFullPolls > 0) {
                 //try to get new cookie:
-                await this.loginToGetNewCookies();
+                this._successFullPolls = 0;
+                await this.cookie.loginToGetNewCookies();
             }
         }
     }
 
-    private async fillIntoObjects(locationData: any): Promise<void> {
+    private async fillIntoObjects(user: User): Promise<void> {
         try {
-            const user = {
-                id: undefined,
-                photoURL: undefined,
-                name: undefined,
-                lat: undefined,
-                long: undefined,
-                address: undefined,
-                battery: undefined,
-                timestamp: undefined,
-                accuracy: undefined,
-            };
-
-            if (locationData && Array.isArray(locationData)) {
-                // locationData present
-                if (locationData[0] && locationData[0][0]) {
-                    user.id = locationData[0][0];
-                }
-                if (locationData[0] && locationData[0][1]) {
-                    user.photoURL = locationData[0][1];
-                }
-                if (locationData[0] && locationData[0][3]) {
-                    user.name = locationData[0][3];
-                }
-                if (locationData[1] && locationData[1][1] && locationData[1][1][2]) {
-                    user.lat = locationData[1][1][2];
-                }
-                if (locationData[1] && locationData[1][1] && locationData[1][1][1]) {
-                    user.long = locationData[1][1][1];
-                }
-                if (locationData[1] && locationData[1][4]) {
-                    user.address = locationData[1][4];
-                }
-                if (locationData[13] && locationData[13][1]) {
-                    user.battery = locationData[13][1];
-                }
-                if (locationData[1] && locationData[1][2]) {
-                    user.timestamp = locationData[1][2];
-                }
-                if (locationData[1] && locationData[1][3]) {
-                    user.accuracy = locationData[1][3];
-                }
-            }
-
             if (user.id) {
-                const basepath = `users.${user.id as string}`;
+                const basepath = `users.${user.id}`;
                 const deviceObj = {
                     _id: basepath,
                     type: 'device',
@@ -307,84 +322,28 @@ class GoogleSharedlocations2 extends utils.Adapter {
         }
     }
 
-    private async loginToGetNewCookies(): Promise<void> {
-        try {
-            if (this._browser) {
-                this.log.info('Seems we are already trying to log in. Aborting new login attempt.');
-                return;
-            }
-            if (!this.config.googleUsername || !this.config.googlePassword) {
-                this.log.warn('Google username or password not set in adapter configuration. Can not login.');
-                return;
-            }
-
-            this.log.info('Trying to login to Google to get new cookies.');
-            this._successFullPolls = 0;
-
-            //testing puppeteer:
-            this.log.debug('Starting browser.');
-            this._browser = await puppeteer.launch({
-                headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled'],
-                ignoreDefaultArgs: ['--enable-automation'], //hide automation flag, did not help.
+    private async notifyPlaces(user: User): Promise<void> {
+        if (this.config.placesInstance && user.id && user.lat && user.long) {
+            await this.sendToAsync(this.config.placesInstance, {
+                user: user.name,
+                latitude: user.lat,
+                longitude: user.long,
+                timestamp: user.timestamp || Date.now(),
+                address: user.address,
             });
-            this.log.debug('browser started, opening new page.');
-            const page = await this._browser.newPage();
+        }
+    }
 
-            //hide puppeteer automation flag
-            await page.evaluateOnNewDocument(() => {
-                Object.defineProperty(navigator, 'webdriver', { get: () => false });
-            });
-            await page.setUserAgent({
-                userAgent:
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-            });
-
-            this.log.debug('going to google login page.');
-            await page.goto(
-                'https://accounts.google.com/ServiceLogin?hl=de&continue=https://www.google.com/maps&gae=cb-eomtm',
-                {
-                    waitUntil: 'networkidle2',
-                    timeout: 60000,
-                },
-            );
-
-            this.log.debug('filling in username and clicking next.');
-            await page.locator('#identifierId').fill(this.config.googleUsername);
-            //is this enough, or do we need to search button in this div?
-            await page.locator('#identifierNext').click();
-            //waiting for #password fails in headles.. :-(
-            await page.waitForNetworkIdle({ idleTime: 2000 });
-
-            this.log.debug('filling in password and clicking next.');
-            //do we need to  wait until page is loaded / rendered here?
-            await page.locator('input[type="password"]').fill(this.config.googlePassword);
-            this.log.debug('clicking password next button.');
-            await page.locator('#passwordNext').click();
-            //await page.waitForNetworkIdle({ idleTime: 2000 }); -> does never happen in headless.. :-/
-            await new Promise(resolve => setTimeout(resolve, 3000));
-
-            await page.goto('https://www.google.com/maps');
-            this.log.debug('getting cookies.');
-            //using deprecated function, but browser.cookies just does not work...???
-            const cookies = await page.cookies();
-
-            this._cookies = cookies
-                .filter(c => c.domain.includes('google'))
-                .map(c => `${c.name}=${c.value}`)
-                .join('; ');
-            //this.log.debug(this._cookies);
-            //console.log(this._cookies);
-            await this._browser.close();
-            if (this._cookies.length < 50) {
-                this.log.warn('Cookie string seems too short, login probably failed!');
-            } else {
-                this.log.info('Obtained new cookies from Google login.');
-                await this.setState('info.currentCookies', { val: this._cookies, ack: true });
+    private async checkFences(user: User): Promise<void> {
+        for (const fence of this.fences) {
+            if (fence.valid && fence.user === user.id) {
+                const inside = fence.isInsideFence(user);
+                await this.setStateChangedAsync(`fences.${fence.fenceId}`, {
+                    val: inside,
+                    ts: user.timestamp,
+                    ack: true,
+                });
             }
-            this._browser = null;
-        } catch (e) {
-            this.log.error(`Error in puppeteer: ${(e as Error).message}`);
         }
     }
 
@@ -393,7 +352,7 @@ class GoogleSharedlocations2 extends utils.Adapter {
      *
      * @param callback - Callback function
      */
-    private onUnload(callback: () => void): void {
+    private async onUnload(callback: () => void): Promise<void> {
         try {
             // Here you must clear all timeouts or intervals that may still be active
             // clearTimeout(timeout1);
@@ -403,13 +362,7 @@ class GoogleSharedlocations2 extends utils.Adapter {
             if (this._pollTimeout) {
                 clearTimeout(this._pollTimeout);
             }
-            if (this._browser) {
-                //ignore results here.
-                this._browser
-                    .close()
-                    .then(() => {})
-                    .catch(() => {});
-            }
+            await this.cookie.cleanUp();
             callback();
         } catch (error) {
             this.log.error(`Error during unloading: ${(error as Error).message}`);
@@ -442,35 +395,46 @@ class GoogleSharedlocations2 extends utils.Adapter {
         if (id.endsWith('info.currentCookies') && state && !state.ack) {
             if (state.val === '') {
                 this.log.info('Current cookies state was cleared, trying to obtain new cookies.');
-                this._cookies = null;
-                await this.loginToGetNewCookies();
-                if (this._cookies) {
+                this._successFullPolls = 0;
+                await this.cookie.loginToGetNewCookies();
+                if (this.cookie.isValid()) {
                     await this.sendRequest();
                 }
             } else {
                 this.log.info(
                     'Current cookies state was changed from outside the adapter, updating internal cookie store.',
                 );
-                this._cookies = state.val as string;
+                this.cookie.currentCookie = state.val as string;
             }
         }
     }
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  */
-    //
-    // private onMessage(obj: ioBroker.Message): void {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
+
+    /**
+     * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+     * Using this method requires "common.messagebox" property to be set to true in io-package.json
+     *
+     * @param obj - message object
+     */
+    private onMessage(obj: ioBroker.Message): void {
+        this.log.debug(`Received ${obj?.command} message`);
+        if (obj?.command === 'getUsers') {
+            this.log.debug('Received getUsers message');
+            // Send response in callback if required
+            if (obj.callback) {
+                try {
+                    const result = Object.values(this._users).map(user => ({
+                        value: user.id,
+                        label: user.name || user.id,
+                    }));
+                    this.log.debug(`Result: ${JSON.stringify(result)}`);
+                    this.sendTo(obj.from, obj.command, result, obj.callback);
+                } catch (e) {
+                    this.log.error(`Error processing getUsers message: ${(e as Error).message}`);
+                    this.sendTo(obj.from, obj.command, [], obj.callback);
+                }
+            }
+        }
+    }
 }
 //if (require.main !== module) {
 // Export the constructor in compact mode
