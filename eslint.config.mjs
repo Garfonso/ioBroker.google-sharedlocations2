d515ccba96dd6c340a43f98aa3a0f2200872f7c7
Garfonso <garfonso@mobo.info>
2026-06-23 15:38:30 +0200

    fix linting
    no linting of test files for now -> there is only the default stub.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default [
         ignores: [
             '.dev-server/',
             '.vscode/',
+            '**/*.test.ts',
             //'*.test.js',
             //'test/**/*.js',
             '*.config.mjs',
