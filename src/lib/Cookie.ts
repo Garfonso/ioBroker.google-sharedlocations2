c0d1157e2515f919dd358723b4b3d7d66d3cdf1e
Garfonso <garfonso@mobo.info>
2026-06-23 15:04:39 +0200

    update puppeteer

--- src/lib/Cookie.ts
+++ src/lib/Cookie.ts
@@ -247,7 +247,7 @@ export class Cookie {
         });
         await page.setUserAgent({
             userAgent:
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36',
         });
 
         return page;
